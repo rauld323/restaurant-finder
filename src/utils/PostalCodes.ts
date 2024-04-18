145987a0848cb7e57fc72c8c8691b2c89f106cfd
Raul Davila <rauldavila323@gmail.com>
2024-04-18 23:48:00 +0200

    create a PostalCode array

---
+++ src/utils/PostalCodes.ts
@@ -0,0 +1,20 @@
+const PostalCodes = [
+  "CT1 2EH",
+  "BS1 4DJ",
+  "L4 0TH",
+  "NE9 7TY",
+  "SW1A 1AA",
+  "CF11 8AZ",
+  "M16 0RA",
+  "EH1 1RE",
+  "BN1 1AE",
+  "CB7 4DL",
+  "LS2 7HY",
+  "G3 8AG",
+  "PL4 0DW",
+  "B26 3QJ",
+  "DH4 5QZ",
+  "BT7 1NN",
+];
+
+export default PostalCodes;
