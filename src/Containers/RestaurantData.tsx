241ee069174b0ce16645854efd6e270954499eec
Raul Davila <rauldavila323@gmail.com>
2024-04-18 23:46:54 +0200

    begin working on form that will allow user input a postcode

--- src/Containers/RestaurantData.tsx
+++ src/Containers/RestaurantData.tsx
@@ -1,12 +1,40 @@
+import { SetStateAction, useState } from "react";
 import styled from "styled-components";
 import RestaurantFrame from "../components/RestaurantFrame";
 import useRestaurantData, { RestaurantProps } from "../hooks/useRestaurantData";
 
 const RestaurantData = () => {
-  const { data: restaurants, isLoading, error } = useRestaurantData("NE97TY");
+  const [postCode, setPostCode] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleInputChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setPostCode(event.target.value);
+    setSubmitted(false);
+  };
+
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    if (postCode.trim() !== "") {
+      setSubmitted(true);
+    }
+  };
+
+  const {
+    data: restaurants,
+    isLoading,
+    error,
+  } = useRestaurantData(postCode, submitted);
 
   return (
     <StyledRestaurantContainer>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={postCode} onChange={handleInputChange} />
+        <button type="submit" disabled={postCode.trim() === ""}>
+          Submit
+        </button>
+      </form>
       <StlyedRestaurantWrapper>
         {isLoading && <h1>Loading...</h1>}
         {restaurants &&
@@ -30,16 +58,15 @@ const RestaurantData = () => {
 export default RestaurantData;
 
 const StyledRestaurantContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 50px;
   min-height: 100vh;
   padding-top: calc(2.5rem + (10 - 7.5) * ((100vw - 20rem) / (100 - 20)));
   padding-bottom: calc(2.5rem + (10 - 7.5) * ((100vw - 20rem) / (100 - 20)));
   padding-left: calc(1rem + (8 - 1) * ((100vw - 20rem) / (100 - 20)));
   padding-right: calc(1rem + (8 - 1) * ((100vw - 20rem) / (100 - 20)));
-  @media screen and (min-width: 500px) {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
 `;
 
 // width : 390
