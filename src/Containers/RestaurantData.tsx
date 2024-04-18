74eee2716a2b88f34e2706c22cf3fd9d7a27db15
Raul Davila <rauldavila323@gmail.com>
2024-04-18 16:49:01 +0200

    create RestaurantFrame component and install styled-component

--- src/Containers/RestaurantData.tsx
+++ src/Containers/RestaurantData.tsx
@@ -1,19 +1,20 @@
+import RestaurantFrame from "../components/RestaurantFrame";
 import useRestaurantData, { RestaurantProps } from "../hooks/useRestaurantData";
 
 const RestaurantData = () => {
-  const { data: info } = useRestaurantData("NE97TY");
+  const { data: restaurants } = useRestaurantData("NE97TY");
   return (
     <>
-      {info &&
-        info.map((data: RestaurantProps) => (
-          <div>
-            <h1>{data.name}</h1>
-            <h2>{data.rating.starRating}</h2>
-            <h3>{data.cuisines.map((cuisine) => cuisine.name)}</h3>
-            <h4>{data.address.city}</h4>
-            <h4>{data.address.firstLine}</h4>
-            <h4>{data.address.postalCode}</h4>
-          </div>
+      {restaurants &&
+        restaurants.map((data: RestaurantProps) => (
+          <>
+            <RestaurantFrame
+              name={data.name}
+              cuisines={data.cuisines}
+              rating={data.rating}
+              address={data.address}
+            />
+          </>
         ))}
     </>
   );
---
+++ src/components/RestaurantFrame.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import styled from "styled-components";
+import { RestaurantProps } from "../hooks/useRestaurantData";
+
+const RestaurantFrame: FC<RestaurantProps> = ({ name, rating, address }) => {
+  return (
+    <StyledFrameContainer>
+      <StyledTopWrapper>
+        <div>
+          <span>{name}</span> <span>{address.city}</span>
+        </div>
+        <div>{rating.starRating}</div>
+      </StyledTopWrapper>
+
+      <StyledBottomWrapper>Pills</StyledBottomWrapper>
+    </StyledFrameContainer>
+  );
+};
+
+export default RestaurantFrame;
+
+const StyledFrameContainer = styled.div`
+  border-radius: 10px;
+  border: 1px solid black;
+`;
+
+const StyledTopWrapper = styled.div`
+  display: flex;
+  padding: 10px;
+`;
+
+const StyledBottomWrapper = styled.div`
+  padding: 10px;
+`;
